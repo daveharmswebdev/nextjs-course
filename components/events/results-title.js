129e2b65eb768dfe788bac822b4f468ec0838caa
Dave Harms <dave.harms@fortyau.com>
2022-12-27 06:11:25 -0600

    completed

---
+++ components/events/results-title.js
@@ -0,0 +1,20 @@
+import classes from './results-title.module.css';
+import ButtonLink from '../ui/button-link';
+
+function ResultsTitle(props) {
+  const { date } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    month: 'long',
+    year: 'numeric',
+  });
+
+  return (
+    <section className={classes.title}>
+      <h1>Events in {humanReadableDate}</h1>
+      <ButtonLink link="/events">Show all events</ButtonLink>
+    </section>
+  );
+}
+
+export default ResultsTitle;
--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,14 +1,66 @@
 import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import { Fragment } from 'react';
+import ResultsTitle from '../../components/events/results-title';
+import ErrorAlert from '../../components/ui/error-alert';
+import ButtonLink from '../../components/ui/button-link';
 
 function FilteredEvents() {
   const router = useRouter();
 
-  console.log(router.query.slug);
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...s</p>;
+  }
+
+  const numYear = +filterData[0];
+  const numMonth = +filterData[1];
+
+  console.log(numYear, numMonth);
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <Fragment>
+        <ErrorAlert>Invalid filter. Please adjust your values!</ErrorAlert>
+        <div className="center">
+          <ButtonLink link="/events">Show All Events</ButtonLink>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>No events found for the chose filter!</ErrorAlert>
+        <div className="center">
+          <ButtonLink link="/events">Show All Events</ButtonLink>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
 
   return (
-    <div>
-      <h1>Filtered Events</h1>
-    </div>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList events={filteredEvents} />
+    </Fragment>
   );
 }
 
